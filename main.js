6136d2ccdad8e7233b76fc0ffe96805f99141606
Mateus Sikacz <108426663+sikacz30@users.noreply.github.com>
2023-10-05 09:09:01 -0300

    05/10/23

--- main.js
+++ main.js
@@ -1,1 +1,30 @@
-alert('bem vindo');
+alert('cesb');
+
+
+document.querySelector('.tecla_pom').onclick = tocaSomPom
+function tocaSomPom(){ document.querySelector( '#som_tecla_pom').play();
+}
+document.querySelector('.tecla_tim').onclick = tocaSomTim
+function tocaSomTim(){ document.querySelector( '#som_tecla_tim').play();
+}
+document.querySelector('.tecla_puff').onclick = tocaSomPuff
+function tocaSomPuff(){ document.querySelector( '#som_tecla_puff').play();
+}
+document.querySelector('.tecla_splash').onclick = tocaSomSplash
+function tocaSomSplash(){ document.querySelector( '#som_tecla_splash').play();
+}
+document.querySelector('.tecla_toim').onclick = tocaSomToim
+function tocaSomToim(){ document.querySelector( '#som_tecla_toim').play();
+}
+document.querySelector('.tecla_psh').onclick = tocaSomPsh
+function tocaSomPsh(){ document.querySelector( '#som_tecla_Psh').play();
+}
+document.querySelector('.tecla_tic').onclick = tocaSomTic
+function tocaSomTic(){ document.querySelector( '#som_tecla_tic').play();
+}
+document.querySelector('.tecla_tom').onclick = tocaSomTom
+function tocaSomTom(){ document.querySelector( '#som_tecla_tom').play();
+}
+document.querySelector('.tecla_clap').onclick = tocaSomClap
+function tocaSomClap(){ document.querySelector( '#som_tecla_Clap').play();
+}
